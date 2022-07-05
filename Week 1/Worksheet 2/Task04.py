35d22b076042ae16d3e5c0b5232b0db9f2e1c944
RoswellCityUK <93608245+RoswellCityUK@users.noreply.github.com>
2022-07-05 22:59:15 +0100

    Update Task04.py
    I just found out that you shouldn't use uppercase letters in a variable name in Python.

--- Week 1/Worksheet 2/Task04.py
+++ Week 1/Worksheet 2/Task04.py
@@ -1,17 +1,17 @@
 import datetime
 
-startTimeHours = int(input("Starting time (hours): "))
-startTimeMinutes = int(input("Starting time (minutes): "))
+start_time_hours = int(input("Starting time (hours): "))
+start_time_minutes = int(input("Starting time (minutes): "))
 
-startTime = datetime.datetime(1,1,1,startTimeHours, startTimeMinutes)
+start_time = datetime.datetime(1,1,1,start_time_hours, start_time_minutes)
 
-eventDuration = int(input("Event duration (minutes): "))
+event_duration = int(input("Event duration (minutes): "))
 
-endTime = startTime + datetime.timedelta(seconds=eventDuration * 60)
-durationDays = (endTime - startTime).days
+end_time = start_time + datetime.timedelta(seconds=event_duration * 60)
+duration_days = (end_time - start_time).days
 
-print("End time = {:d}:{:02d}".format(endTime.hour, endTime.minute), end="")
-if endTime.day == 2 and durationDays <= 1:
+print("End time = {:d}:{:02d}".format(end_time.hour, end_time.minute), end="")
+if end_time.day == 2 and duration_days <= 1:
     print(", the next day")
-elif durationDays >= 1:
-    print(",", durationDays + 1, "days later")
+elif duration_days >= 1:
+    print(",", duration_days + 1, "days later")
