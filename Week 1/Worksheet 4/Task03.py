c11fb5f2fdd09054aff09b3fb77a628e89a7d5ae
RoswellCityUK <93608245+RoswellCityUK@users.noreply.github.com>
2022-07-07 23:07:49 +0100

    Update Task03.py
    forgot about last requirement

--- Week 1/Worksheet 4/Task03.py
+++ Week 1/Worksheet 4/Task03.py
@@ -1,6 +1,6 @@
 """
 Program generates two list one with 10 odd number and second one with 10 even numbers
-After that is generating one new list from both previous list in DESC order
+After that is generating one new list from both previous list in DESC order and print 3 first numbers
 """
 
 newlist1 = [x for x in range(10*2) if x % 2 != 0]
@@ -15,4 +15,4 @@ for x in range(len(newlist1)-1, -1, -1):
     newlist3.append(newlist1[x])
     newlist3.append(newlist2[x])
 
-print(newlist3)
+print(newlist3[:3])
