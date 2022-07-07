03eb68f8da8577cd4b5857c3374167a13a5f032f
RoswellCityUK <93608245+RoswellCityUK@users.noreply.github.com>
2022-07-07 22:44:59 +0100

    Create Task03.py

---
+++ Week 1/Worksheet 4/Task03.py
@@ -0,0 +1,18 @@
+"""
+Program generates two list one with 10 odd number and second one with 10 even numbers
+After that is generating one new list from both previous list in DESC order
+"""
+
+newlist1 = [x for x in range(10*2) if x % 2 != 0]
+print(newlist1, f"(len = {len(newlist1)})")
+
+newlist2 = [x for x in range(10*2) if x % 2 == 0]
+print(newlist2, f"(len = {len(newlist2)})")
+print()
+
+newlist3 = []
+for x in range(len(newlist1)-1, -1, -1):
+    newlist3.append(newlist1[x])
+    newlist3.append(newlist2[x])
+
+print(newlist3)
