ba0ad836aa2c9a081d5a64938763156516a50bc0
RoswellCityUK <93608245+RoswellCityUK@users.noreply.github.com>
2022-07-07 14:26:01 +0100

    Create Task02.py

---
+++ Week 1/Worksheet 3/Task02.py
@@ -0,0 +1,20 @@
+"""
+Programming is asking User to enter his name and integer number
+then print this name given number of times using While and For loops
+"""
+
+user_name = input("Enter your name: ")
+user_number = int(input("Enter integer number: "))
+
+counter = 0
+
+print()
+print("Printing name with while loop:")
+while counter < user_number:
+    print(user_name, end=" ")
+    counter += 1
+print("\n")
+print("Printing name with for loop")
+for x in range(user_number):
+    print(user_name, end=" ")
+print()
