94458652561768954d3cc8943c290a4a9f40201c
RoswellCityUK <93608245+RoswellCityUK@users.noreply.github.com>
2022-07-08 20:47:20 +0100

    Update Task01.py

--- Week 1/Worksheet 7/Task01.py
+++ Week 1/Worksheet 7/Task01.py
@@ -1,22 +1,15 @@
 """
-This piece of software is calculating factorial of a number using recursive function
-with exception handling
+Target here was to add Exception handling to the provided code
 """
 
+food = ["chocolate", "chicken", "corn", "sandwich", "soup", "potatoes", "beef", "lemonade"]
 
-def calc_factorial(number):
+fifthLetter = []
+
+for x in food:
     try:
-        if number <= 1:
-            return 1
-        else:
-            return number * calc_factorial(number-1)
-    except:
-        print("Wrong argument. Type in integer as an function argument")
-        exit()
+        fifthLetter.append(x[4])
+    except IndexError:
+        print("A word less than 5 characters long was encountered")
 
-
-print("0! =", calc_factorial(0))
-print("1! =", calc_factorial(1))
-print("3! =", calc_factorial(3))
-print("5! =", calc_factorial(5))
-print("Three! =", calc_factorial('Three'))
+print(fifthLetter)
