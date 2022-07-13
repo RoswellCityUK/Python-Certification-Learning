7b2132c8d66bf4b98cae1a8c0394679c2360cb8c
RoswellCityUK <93608245+RoswellCityUK@users.noreply.github.com>
2022-07-13 18:43:32 +0100

    Create Task02.py

---
+++ Week 2/Worksheet 3/Task02.py
@@ -0,0 +1,31 @@
+"""
+Program should include function which analyse the provided number [int: 1-10].
+    - If number is outside the range -> raise ArithmeticError and re-raise after handling in function
+    - If string is provided -> ValueError and handled only in caller
+    - If no exceptions -> print "Everything is beautiful."
+Program should end with "End of code" regardless of the exceptions.
+"""
+
+
+def analyse(number):
+    a = ArithmeticError('Error: Number outside the range')
+    try:
+        number = float(number)
+        if number < 0 or number > 10:
+            raise a
+    except ArithmeticError:
+        print("ArithmeticError handled in function")
+        raise a
+
+
+user_number = input("Enter number: ")
+
+try:
+    analyse(user_number)
+except ValueError:
+    print('ValueError handled by caller')
+except ArithmeticError:
+    print('ArithmeticError re-handled by caller')
+else:
+    print('Everything is beautiful.')
+print('End of code.')
